0afcde6bbc5aaf85d1dc02ef0d8c842d1b48725c
TrungNgo21 <90534385+TrungNgo21@users.noreply.github.com>
2021-09-12 06:59:47 +0700

    Add files via upload

---
+++ jss/popup.js
@@ -0,0 +1,92 @@
+﻿
+//BẠN SỬA MÃNG BÊN DƯỚI, MỖI PHẦN TỬ LÀ MỘT KÍ TỰ TÍNH LUỐN KÍ TỰ TRẮNG NHÉ! BẠN CÓ THỂ THÊM NHIỀU PHẦN TỬ HƠN KHÔNG NHẤT THIẾT LÀ 28
+//XEM KĨ VIDEO CỦA MÌNH HƠN Ở PHẦN NÀY NHÉ!
+
+status="Quốc Định"
+var a=new Array(),n=""
+a[1]='Đ';
+a[2]='ơ';
+a[3]='n';
+a[4]=' ';
+a[5]='g';
+a[6]='i';
+a[7]='ả';
+a[8]='n';
+a[9]='!';
+a[10]=' ';
+a[11]='V';
+a[12]='ì';
+a[13]=' ';
+a[14]='B';
+a[15]='ố';
+a[16]=' ';
+a[17]='M';
+a[18]='à';
+a[19]='y';
+a[20]=' ';
+a[21]='T';
+a[22]='h';
+a[23]='í';
+a[24]='c';
+a[25]='h';
+a[26]='!';
+
+function one()
+{
+t=document.f.txt.value
+j=t.length
+if(j>0)
+{
+for(var i=1;i<=j;i++)
+{
+n=n+a[i]
+if(i==27)
+{
+document.f.txt.value=""
+n=""
+}
+}
+}
+document.f.txt.value=n
+n=""
+setTimeout("one()",1)
+}
+function s()
+{
+}
+function on()
+{
+one()
+}
+
+        $(document).ready(function() {
+            $('#k').hide();
+            $('h1').click(function() {
+                $('.active').removeClass('active');
+                $('#k').slideUp('fast');
+                if($(this).next('#k').is(':hidden') == true) {
+                $(this).addClass('active');
+                $(this).next('#k').slideDown('fast');
+                }
+            });
+        });
+					function Yeu()
+					{
+					$("#divResult").fadeOut(0);
+					//PHẦN NÀY LÀ PHẦN HIỆN KHI BẠN BẤM "GỞI ĐI" LINK Ở SAU LÀ ICON MẶT CƯỜI XÓA BỎ CŨNG ĐƯỢC CÒN TEXT THÌ BẠN SỬA LẠI THEO Ý CỦA BẠN.
+					$("#divResult").html("</br><h2>T đã đoán trc đc thế nào nó cũng trả lời thế này</h2>");
+					$("#divResult").fadeIn(2000,function()
+							{
+							$("#divResult2").fadeOut(0);
+							$("#divResult2").html("<p>Thế thì cho làm nóc nhé chịu ko =))).</p></br>");
+							$("#divResult2").fadeIn(2000,function()
+									{
+									$("#divResult3").fadeOut(0);
+									$("#divResult3").html("<p>Chịu trách nhiệm với hành vi của mình đi nhé =)))) </p></br>");
+									$("#divResult3").fadeIn(2000);
+									}
+								);
+							}
+						);
+					}
+				
